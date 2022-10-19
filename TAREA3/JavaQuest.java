7f3cba4015b4e1c4ed071c597fa141ed92c41aef
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-10-19 19:54:16 -0300

    [ADD] getting final boss and converting SortedSet to an array for add to adyadence matrix

--- TAREA3/JavaQuest.java
+++ TAREA3/JavaQuest.java
@@ -3,20 +3,10 @@ import java.util.*;
 
 public class JavaQuest {
     public static void main(String[] args) {
-        System.out.print("Profundidad del mapa\n> ");  
+        System.out.print("Profundidad del mapa (mayor estricto a 2)\n> ");  
         Scanner sc= new Scanner(System.in);
         int profundidad = sc.nextInt();
-        SortedSet<Edge> edges = GraphGenerator.Generar(profundidad);
-        Mapa mapa = new Mapa(profundidad*profundidad);
-
-        for (Edge e : edges) {
-            // System.out.printf("\n(%d) -> (%d)", e.x, e.y);
-            mapa.agregar(e.x, e.y);
-        }
-
+        Mapa mapa = new Mapa(profundidad);
         mapa.verMapa();
-
-
-
     }
 }
--- TAREA3/components/Mapa.java
+++ TAREA3/components/Mapa.java
@@ -1,17 +1,27 @@
 package components;
 import components.Nodo;
 import components.NodoInicial;
+import components.NodoEvento;
+import components.NodoTienda;
+import components.NodoCombate;
+import components.NodoJefeFinal;
+import components.Edge;
+import java.util.HashMap;
+import java.util.SortedSet;
 
 public class Mapa {
 	private int profundidad;
-	private NodoInicial nodo_inicial;
+	private NodoInicial nodo_inicial = new NodoInicial();
 	private Nodo nodo_actual;
-
+	private HashMap<Integer, String> tipos_nodos = new HashMap<Integer, String>();
+	private int[] probabilidades = new int[] {1, 1, 1, 2, 3, 3, 3, 3, 3, 3};
 	private int n;
     private int[][] matriz;
+    private NodoJefeFinal jefe = new NodoJefeFinal("JEFE FINAL");
 
-    public Mapa(int n) {
-        this.n = n;
+    public Mapa(int profundidad ) {
+        this.n = profundidad*profundidad;
+        this.profundidad = profundidad;
         matriz = new int[this.n][this.n];
         //se inicializa matriz en 0
         for(int i = 0; i < n; i++){
@@ -19,10 +29,20 @@ public class Mapa {
                 matriz[i][j] = 0;
             }            
         }
+        SortedSet<Edge> edges = GraphGenerator.Generar(profundidad);
+        // get last elements from the sortedset
+        Edge[] edges_array = edges.toArray(new Edge[edges.size()]);
+        jefe.set_id(edges_array[edges_array.length-1].y);
+        
+        for (int i = 0; i < nodo_jefe_final; i++) {
+        	this.agregar(edges_array[i].x, edges_array[i].y);
+        }
     }
     
     public void agregar(int i, int j){
-        matriz[i][j] += 1;
+    	for (int k = 1; k < this.n; k++) {
+    		matriz[i][j] = probabilidades[numero_aleatorio(0, 10)];
+    	}
     }
     
     public void remover(int i, int j){
@@ -32,23 +52,28 @@ public class Mapa {
     
 
     public void imprimir_siguientes(int n) {
-    	System.out.print(n + " | ");
+    	System.out.printf("%-7d | ", n);
     	for (int i = 0; i < this.n; i++) {
-    		System.out.print(matriz[n][i] + "  " );
+    		System.out.printf("%-7d  ", matriz[n][i]);
     	}
     }
 
     public void verMapa(){
         for(int i = 0; i < this.n; i++){
-        	System.out.print(i + " | ");
+        	System.out.printf("%-2d | ", i);
             for(int j = 0; j < n; j++){
-                System.out.print(matriz[i][j] + "  " );        
+                System.out.printf("%-2d  ", matriz[i][j]);       
             }
             System.out.println();
         }  
     }
 
+	public int numero_aleatorio(int min, int max) {
+    	return (int) ((Math.random() * (max - min)) + min);
+	}
+
 	public void avanzar() {
 		nodo_actual.set_id(nodo_actual.get_id() + 1);
 	}
+
 }
