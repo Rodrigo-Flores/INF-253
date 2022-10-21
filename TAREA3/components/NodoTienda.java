c4120f85f6cceae32e8ef2987e4712d10882b770
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-10-20 22:53:32 -0300

    [ADD] comprar and interactuar methods ready (for checking)

--- TAREA3/components/NodoTienda.java
+++ TAREA3/components/NodoTienda.java
@@ -1,13 +1,37 @@
 package components;
 import components.Nodo;
 import components.Item;
+import components.Jugador;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class NodoTienda extends Nodo {
+	private String nombre;
 	private List<Item> items = new ArrayList<Item> ();
 
-	public void interactuar() {}
-	public void agregarNodo(Nodo nodo) {}
-	public void comprar(int n) {}
+	public NodoTienda (String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void interactuar() {
+		System.out.println("Items disponibles para comprar:");
+		for (int i = 0; i < this.items.size(); i++) {
+			System.out.printf("(%i) Nombre: %s -> Precio: %i\n", i, this.items.get(i).get_nombre(), this.items.get(i).get_precio());
+		}
+	}
+	public void comprar(int n, Jugador jugador) {
+		int eleccion = 0;
+		while (eleccion == 0) {
+			if (jugador.get_dinero() >= this.items.get(n).get_precio()) {
+				jugador.asignarItem(this.items.get(n));
+			} else {
+				System.out.println("No tienes el dinero suficiente para comprar el item.");
+			}
+			System.out.println("Elige una opcion:");
+			System.out.print("(0) Comprar.\n(1) Salir.");
+			Scanner input = new Scanner(System.in);
+			eleccion = input.nextInt();
+		}
+	}
 }
