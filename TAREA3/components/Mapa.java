dbe4968d1b65e34dfeb6c24407928c6edde12829
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-10-20 22:44:03 -0300

    [ADD] correctly final boss identification and correctly random nodes.

--- TAREA3/components/Mapa.java
+++ TAREA3/components/Mapa.java
@@ -11,85 +11,52 @@ import java.util.SortedSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class Mapa {
 	private int profundidad;
 	private NodoInicial nodo_inicial = new NodoInicial();
-	private Nodo nodo_actual;
+	private Nodo nodo_actual = nodo_inicial;
 	private HashMap<Integer, String> tipos_nodos = new HashMap<Integer, String>();
-	private int n;
-    private int[][] matriz;
-    private NodoJefeFinal jefe = new NodoJefeFinal("JEFE FINAL");
+    private int nodo_jefe;
+    private SortedSet<Edge> edges;
 
     public Mapa(int profundidad ) {
-        this.n = profundidad*profundidad;
         this.profundidad = profundidad;
-        matriz = new int[this.n][this.n];
-        //se inicializa matriz en 0
-        for(int i = 0; i < this.n; i++){
-            for(int j=0; j< this.n; j++){
-                matriz[i][j] = 0;
-            }            
-        }
-        SortedSet<Edge> edges = GraphGenerator.Generar(profundidad);
-        for (Edge edge : edges) {
-        	if (edge.x == 0) {
-        		double seleccion = Math.random();
-        		if (seleccion < 0.3) {
-        			NodoEvento evento = new NodoEvento("EVENTO");
-        			evento.set_id(edge.y);
-        			nodo_inicial.agregarNodo(evento);
-        		} else if (seleccion < 0.4) {
-        			NodoTienda tienda = new NodoTienda();
-        			tienda.set_id(edge.y);
-        			nodo_inicial.agregarNodo(tienda);
-        		} else if (seleccion < 1) {
-        			NodoCombate combate = new NodoCombate("ENEMIGO");
-        			combate.set_id(edge.y);
-        			nodo_inicial.agregarNodo(combate);
-
-        		}
-        	}
-        }
-
-
-    }
-    
-    public void agregar(int i, int j){
-    	for (int k = 1; k < this.n; k++) {
-    		matriz[i][j] += 1;
-    	}
-    }
-    
-    public void remover(int i, int j){
-        if(matriz[i][j] > 0)
-            matriz[i][j] -= 1;
-    }
-    
-
-    public void imprimir_siguientes(int n) {
-    	System.out.printf("%-2d | ", n);
-    	for (int i = 0; i < this.n; i++) {
-    		System.out.printf("%-2d  ", matriz[n][i]);
-    	}
+        this.edges = GraphGenerator.Generar(profundidad);
+        this.nodo_jefe = edges.last().y;
     }
 
     public void verMapa(){
-        for(int i = 0; i < this.n; i++){
-        	System.out.printf("%-2d | ", i);
-            for(int j = 0; j < n; j++){
-                System.out.printf("%-2d  ", matriz[i][j]);       
-            }
-            System.out.println();
-        }  
+    	for (Edge edge : edges) {
+    		System.out.printf("(%d) -> (%d)\n", edge.x, edge.y);
+    	}
     }
 
-	public int numero_aleatorio(int min, int max) {
-    	return (int) ((Math.random() * (max - min)) + min);
-	}
-
 	public void avanzar() {
-		nodo_actual.set_id(nodo_actual.get_id() + 1);
+	Scanner input = new Scanner(System.in);  // Create a Scanner object
+		System.out.println("\nCaminos diposnibles para avanzar:\n");
+		for (Edge edge : edges) {
+			if (edge.x == nodo_actual.get_id()) {
+				System.out.printf("\t(%d)", edge.y);
+			}
+		}
+		System.out.println("\n\nEscoge una alternativa:");
+    	int camino = input.nextInt();
+		if (camino != this.nodo_jefe) {
+	    	double n = Math.random();
+	    	if (n < 0.3) {
+	    		nodo_actual = new NodoEvento("EVENTO");
+	    	} else if (n < 0.4) {
+	    		nodo_actual = new NodoTienda("TIENDA");
+	    	} else if (n < 1.0) {
+	    		nodo_actual = new NodoCombate("ENEMIGO");
+	    	}
+	    	nodo_actual.set_id(camino);
+		} else {
+			nodo_actual = new NodoJefeFinal("JEFE FINAL");
+			System.out.println("JEFE FINAL");
+		}
 	}
 
 }
