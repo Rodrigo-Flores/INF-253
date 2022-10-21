533a577e3cd1ffa816b2b31a5f5d2dd99eb341e3
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-10-21 20:57:28 -0300

    [ADD] user bahavior

--- TAREA3/JavaQuest.java
+++ TAREA3/JavaQuest.java
@@ -3,14 +3,41 @@ import java.util.*;
 
 public class JavaQuest {
     public static void main(String[] args) {
-        System.out.print("Profundidad del mapa (mayor estricto a 2)\n> ");  
         Scanner sc= new Scanner(System.in);
+
+        System.out.print("Profundidad del mapa (mayor estricto a 2)\n> ");  
         int profundidad = sc.nextInt();
         Mapa mapa = new Mapa(profundidad);
-        mapa.verMapa();
-        mapa.avanzar();
-        mapa.avanzar();
-        mapa.avanzar();
-        mapa.avanzar();
+
+        System.out.print("¿Cual es tu nombre?\n> ");
+        String nombre = sc.next();
+        Jugador jugador = new Jugador(nombre);
+
+        int eleccion;
+        boolean flag = true;
+        while (flag) {
+            System.out.println("\nIngrese una opcion:");
+            System.out.print("(1) Avanzar\n(2) Ver mapa\n(3) Ver estadisticas\n(4) Ver items\n(5) Salir\n\n");
+            eleccion = sc.nextInt();
+            switch (eleccion) {
+                case 1:
+                    mapa.avanzar();
+                    break;
+                case 2:
+                    mapa.verMapa();
+                    break;
+                case 3:
+                    jugador.verEstado();
+                    break;
+                case 4:
+                    jugador.verItems();
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("¡Ups! Parece no se puede hacer eso. ¿Y si pruebas de nuevo?");
+            }
+        }
     }
 }
