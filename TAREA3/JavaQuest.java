2e2bfb51b47c5726d211f22f0c8de2ad25dc8687
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-10-21 23:19:37 -0300

    [ADD] console print

--- TAREA3/JavaQuest.java
+++ TAREA3/JavaQuest.java
@@ -16,8 +16,8 @@ public class JavaQuest {
         int eleccion;
         boolean flag = true;
         while (flag) {
-            System.out.println("\nIngrese una opcion:");
-            System.out.print("(1) Avanzar\n(2) Ver mapa\n(3) Ver estadisticas\n(4) Ver items\n(5) Salir\n\n");
+            System.out.print("\n(1) Avanzar\n(2) Ver mapa\n(3) Ver estadisticas\n(4) Ver items\n(5) Salir\n\n");
+            System.out.print("\nIngrese una opcion\n> ");
             eleccion = input.nextInt();
             switch (eleccion) {
                 case 1:
