4d048cc61baab51e06ed23591a1fb73231f2d262
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-10-22 22:56:20 -0300

    [ADD] puchasing complete

--- TAREA3/components/NodoTienda.java
+++ TAREA3/components/NodoTienda.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class NodoTienda extends Nodo {
 	private String nombre;
 	private List<Item> items = new ArrayList<Item> ();
+	private int eleccion;
 
 	public NodoTienda (String nombre) {
 		this.nombre = nombre;
@@ -19,17 +20,43 @@ public class NodoTienda extends Nodo {
 
 	public final void interactuar(Jugador jugador) {
 		System.out.println("Items disponibles para comprar:");
-		for (int i = 0; i < this.items.size(); i++) {
-			System.out.printf("(%i) Nombre: %s -> Precio: %i\n", i, this.items.get(i).get_nombre(), this.items.get(i).get_precio());
+		System.out.printf("%-10s | %-10s | %-14s | %-18s | %-18s | %-18s %n",
+			"Indice",
+			"Precio",
+			"Recuperar HP",
+			"Aumentar HP total",
+			"Aumentar danio",
+			"Aumentar defensa");
+		for (int i = 0; i < (int) this.items.size(); i++) {
+			System.out.printf("%-10d | %-10d | %-14d | %-18d | %-18d | %-18d %n",
+				i,
+				this.items.get(i).get_precio(),
+				this.items.get(i).get_recuperar_hp(),
+				this.items.get(i).get_aumentar_hp_total(),
+				this.items.get(i).get_aumentar_danio(),
+				this.items.get(i).get_aumentar_defensa()
+			);
 		}
 		System.out.print("\nIngresa una opcion\n>");
+		System.out.print("¿Te gustaria comprar algo?\n(y/N) > ");
+		Scanner input = new Scanner(System.in);
+		String comprar;
+		comprar = input.next();
+		if ((comprar.equals("y")) || (comprar.equals("Y"))) {
+			System.out.print("Introduce el indice el item que deseas comprar\n> ");
+			eleccion = input.nextInt();
+			this.comprar(eleccion, jugador);
+		}
 	}
+
 	public void comprar(int n, Jugador jugador) {
-		int eleccion = 0;
+		this.eleccion = 0;
 		while (eleccion == 0) {
 			if (jugador.get_dinero() >= this.items.get(n).get_precio()) {
 				jugador.asignarItem(this.items.get(n));
 				jugador.set_dinero(jugador.get_dinero() - this.items.get(n).get_precio());
+				System.out.println("Item agregado al inventario.");
+				eleccion = 1;
 			} else {
 				System.out.println("No tienes el dinero suficiente para comprar el item.");
 			}
