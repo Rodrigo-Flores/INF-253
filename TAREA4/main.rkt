bf4c93b155983ce3437afab99dd8acd68fa1c6b3
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-11-14 22:55:57 -0300

    [ADD] umbral is working just for a single search

--- TAREA4/main.rkt
+++ TAREA4/main.rkt
@@ -6,8 +6,17 @@
      ((equal? item (car list)) (cdr list))
      (else (cons (car list) (delete item (cdr list)))))))
 
-(define (inverso lista n)
-  
-  ;(map (lambda (x) (delete x (range n))) lista))
+(define index
+  (lambda (elemento lista)
+    (- (length lista) (length (memv elemento lista)))))
 
-  
+(define (umbral_simple_helper lista umbral)
+  (cond ((= 1 (length lista)) (first lista))
+        (else (display (index (first umbral) lista)))))
+
+(define (umbral_simple lista umbral tipo)
+  (cond ((equal? tipo #\M) (umbral_simple_helper lista (filter (lambda (x) (> x umbral)) lista)))
+        ((equal? tipo #\m) (umbral_simple_helper lista (filter (lambda (x) (< x umbral)) lista)))))
+
+
+(umbral_simple '(15 2 1 3 27 5 10) 5 #\M)
