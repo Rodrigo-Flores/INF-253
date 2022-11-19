f7b084c2cd868973ce0e4d69f7a724b5ceb21cd5
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-11-18 23:13:13 -0300

    [ADD] estables is ready and simply recursion for modsel is also ready ;D

--- TAREA4/main.rkt
+++ TAREA4/main.rkt
@@ -45,19 +45,20 @@
 
 ; ---------------------- simple ---------------------- ;
 
-;(define (get-element lst pos) 
-;  (if (<= pos 0)
-;      (car lst)
-;      (get-element (cdr lst) (- pos 1))))
-;
-;(define (modsel_simple_helper lista seleccion f i)
-;  (if (equal? i 0) (car lista)
-;      (if (equal? i 4) (car lista) (cons '() (modsel_simple_helper (cdr lista) seleccion f (- i 1))))))
-;
-;(define (modsel_simple lista seleccion f)
-;  ;(map f (map (lambda (x) (get-element lista x)) seleccion))
-;  (modsel_simple_helper (reverse lista) seleccion f (- (length lista) 1)))
-;
+(define (modsel_simple_helper lista seleccion f i aux)
+  (cond ((not (null? lista))
+         (if (list? (memq i seleccion))
+             (modsel_simple_helper (cdr lista) seleccion f (+ i 1) (cons aux (f (car lista))))
+             (modsel_simple_helper (cdr lista) seleccion f (+ i 1) (cons aux (car lista)))))
+        (else (flatten aux))))
+  ;(if (equal? 1 (length lista)) (flatten aux) '(1)))
+      
+
+(define (modsel_simple lista seleccion f)
+  (modsel_simple_helper lista seleccion f 0 '()))
+
+;(modsel_simple '(15 2 1 3 27 5 10) '(0 4 6) (lambda (x) (modulo x 2)))
+;(modsel_simple '(15 2 1 3 27 5 10) '(3 1 2) (lambda (x) (+ x 5)))
 
 ; ---------------------- cola ---------------------- ;
 (define (modsel_cola_helper lista seleccion f i aux)
@@ -69,5 +70,13 @@
 (define (modsel_cola lista seleccion f)
   (modsel_cola_helper lista seleccion f 0 '()))
 
-(modsel_cola '(15 2 1 3 27 5 10) '(0 4 6) (lambda (x) (modulo x 2)))
+;(modsel_cola '(15 2 1 3 27 5 10) '(0 4 6) (lambda (x) (modulo x 2)))
 ;(modsel_cola '(15 2 1 3 27 5 10) '(3 1 2) (lambda (x) (+ x 5)))
+
+; ---------------------- INICIO ESTABLES ---------------------- ;
+(define (estables lista umbral fM fm)
+  (append
+   (list (length (umbral_simple (modsel_cola lista (umbral_simple lista umbral #\M) fM) umbral #\M)))
+   (list (length (umbral_simple (modsel_cola lista (umbral_simple lista umbral #\m) fm) umbral #\m)))))
+
+;(estables '(15 2 1 3 27 5 10) 5 (lambda (x) (/ x 2)) (lambda (x) (* x 2)))
